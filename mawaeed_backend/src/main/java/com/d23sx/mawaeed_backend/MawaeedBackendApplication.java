1d7083afbde84243749f452960defa82a446239f
dshowait <dana.showaiter23@gmail.com>
2025-11-28 21:32:51 +0300

    first commit

---
+++ mawaeed_backend/src/main/java/com/d23sx/mawaeed_backend/MawaeedBackendApplication.java
@@ -0,0 +1,13 @@
+package com.d23sx.mawaeed_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MawaeedBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MawaeedBackendApplication.class, args);
+    }
+
+}
---
+++ mawaeed_backend/src/test/java/com/d23sx/mawaeed_backend/MawaeedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d23sx.mawaeed_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MawaeedBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
